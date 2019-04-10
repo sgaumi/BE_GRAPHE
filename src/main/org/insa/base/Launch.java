3d9379fb9ea6c99d4907376529c8f8bd2417856b
M. Gaumart Simeon <gaumart@insa-20163>
2019-04-10 16:12:15 +0200

    launch marche

--- src/main/org/insa/base/Launch.java
+++ src/main/org/insa/base/Launch.java
@@ -16,6 +16,7 @@ import org.insa.graph.io.GraphReader;
 import org.insa.graph.io.PathReader;
 import org.insa.graphics.drawing.Drawing;
 import org.insa.graphics.drawing.components.BasicDrawing;
+import org.insa.graph.io.BinaryPathReader;
 
 public class Launch {
 
@@ -46,8 +47,8 @@ public class Launch {
     public static void main(String[] args) throws Exception {
 
         // Visit these directory to see the list of available files on Commetud.
-        String mapName = "/home/corentin/Documents/Travail/3IR/BE Graphe/commetud/insa.mapgr";
-        String pathName = "/home/corentin/Documents/Travail/3IR/BE Graphe/commetud/path_fr31insa_rangueil_r2.path";
+        String mapName = "/home/gaumart/Bureau/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/insa.mapgr";
+        String pathName = "/home/gaumart/Bureau/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Paths/path_fr31insa_rangueil_r2.path";
 
         // Create a graph reader.
         GraphReader reader = new BinaryGraphReader(
