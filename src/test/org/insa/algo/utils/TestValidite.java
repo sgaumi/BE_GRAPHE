c6c2f4e297fbdbe46b12cdb5ff3bebbf55297559
Corentin ENIREVEC <c.eni@laposte.net>
2019-05-23 11:04:48 +0200

    Début JUnit

---
+++ src/test/org/insa/algo/utils/TestValidite.java
@@ -0,0 +1,41 @@
+package org.insa.algo.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+
+public class TestValidite {
+	//Ce dont on aura besoin pour le test
+	
+	//Chargement des cartes
+	//Définition des couples origine/destination
+	//ArcInspector
+	@Before
+	public void initAll() throws IOException {
+		System.out.println("Hi, I'm Cortana");
+		
+		//Initialisation des 
+	}
+	
+	@Test
+	public void testValidite() {
+		
+	}
+}
